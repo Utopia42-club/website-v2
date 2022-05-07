0e116c91cc8f01d8605f6f1ac662ae8e8e65783f
abbas nosrati <abbas.nosrati68@gmail.com>
2022-05-07 18:40:49 +0430

    change video link

--- src/Components/Our Video/OurVideo.jsx
+++ src/Components/Our Video/OurVideo.jsx
@@ -4,10 +4,11 @@ const OurVideo = () => {
     return (
         <div className="video">
             <h1>Watch our Video</h1>
-            <iframe className="videoframe"
-                // width="560" height="315"
-                src="https://www.youtube.com/watch?v=OasbYWF4_S8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe className="videoframe"  src="https://www.youtube.com/embed/OasbYWF4_S8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
+
+
+
     )
 }
 export default OurVideo
