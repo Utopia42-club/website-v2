a564e416014fa21db4b57628f88128245ac1507e
abbas nosrati <abbas.nosrati68@gmail.com>
2022-06-29 11:26:45 +0430

    change config

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,10 +1,6 @@
-const ESLintPlugin = require('eslint-webpack-plugin')
+
 module.exports = {
   future: {
     webpack5: true,
-  },
-  webpack: (config, { dev }) => {
-    if (dev) config.plugins.push(new ESLintPlugin())
-    return config
-  },
+  }
 }
