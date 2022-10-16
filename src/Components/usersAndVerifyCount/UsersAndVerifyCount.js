025b15fe66d34778e9d11b1dc109badc47994cb4
abbas nosrati <abbas.nosrati68@gmail.com>
2022-10-16 21:53:56 +0330

    change style

--- src/Components/usersAndVerifyCount/UsersAndVerifyCount.js
+++ src/Components/usersAndVerifyCount/UsersAndVerifyCount.js
@@ -3,8 +3,8 @@ import useCitizenIDVerifications from '../../hooks/useCitizenIDVerifications'
 import useGetTotalSupply from '../../hooks/useGetTotalSupply'
 
 const UsersAndVerifyCount = () => {
-  const [citizenIDVerifications, setCitizenIDVerifications] = useState()
-  const [totalSupply, setTotalSupply] = useState()
+  const [citizenIDVerifications, setCitizenIDVerifications] = useState('...')
+  const [totalSupply, setTotalSupply] = useState('...')
   const itemsCount = useCitizenIDVerifications()
   const getTotalSupply = useGetTotalSupply()
 
@@ -18,7 +18,7 @@ const UsersAndVerifyCount = () => {
   }, [])
 
   return (
-      <div style={{border:'2px solid #814f8c',padding:'5px 62px', borderRadius:'15px',marginTop:'15px'}}>
+      <div style={{border:'2px solid #814f8c',padding:'3px 48px', borderRadius:'15px',marginTop:'15px', minWidth:'270px'}}>
         <div style={{width:'100%', textAlign:'center', color:'#814f8c'}}>
         <div style={{display:'flex', alignItems:'center', justifyContent:'center', columnGap: '10px', marginTop:'5px', marginBottom:'5px'}}>
         Utopia42 Citizens:
